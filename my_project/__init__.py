e98a6acc237a03e30e18c55d3050750598c3eb2a
MithRa1n <85968501+MithRa1n@users.noreply.github.com>
2025-10-09 22:54:54 +0300

    check

--- my_project/__init__.py
+++ my_project/__init__.py
@@ -295,7 +295,7 @@ def _init_swagger(app: Flask) -> None:
     
     api = Api(
         app, 
-        title='Pizza Delivery Management API',
+        title='Pizza Delivery Management API 228',
         description='Complete REST API for pizza delivery management system with authentication',
         version='2.0',
         doc='/api/docs/',
